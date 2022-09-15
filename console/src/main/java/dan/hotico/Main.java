454758895b92cab38f8543d0c9fcb9eab467d5b8
Dan-H00 <hotico.dan00@yahoo.com>
2022-09-16 01:03:56 +0300

    added spring boot configuration to project

--- console/src/main/java/dan/hotico/Main.java
+++ console/src/main/java/dan/hotico/Main.java
@@ -1,21 +1,20 @@
-package dan.hotico.console;
+package dan.hotico;
 
 import dan.hotico.config.GameConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
-        // create context (container)
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
-
-        // close context (container)
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
