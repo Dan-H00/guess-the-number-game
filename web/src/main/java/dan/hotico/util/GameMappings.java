e9e82d876e821271dac7d2db8b70bf2c94b934ab
Dan-H00 <hotico.dan00@yahoo.com>
2022-09-16 20:19:34 +0300

    added thymeleaf integration and web app

---
+++ web/src/main/java/dan/hotico/util/GameMappings.java
@@ -0,0 +1,10 @@
+package dan.hotico.util;
+
+public final class GameMappings {
+    // == constants ==
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    // == constructors ==
+    private GameMappings() {}
+}
---
+++ web/src/main/java/dan/hotico/util/ViewNames.java
@@ -0,0 +1,11 @@
+package dan.hotico.util;
+
+public final class ViewNames {
+    // == constants ==
+    public static final String HOME = "home";
+    public static final String GAME_OVER = "game-over";
+    public static final String PLAY = "play";
+
+    // == constructors ==
+    private ViewNames() {}
+}
